d0123699257001033df27591529e847b3ae286f6
Juan Pablo Parra <jpparrac@gmail.com>
2022-03-10 16:51:08 -0700

    add function icp() in exerciseone.py

--- ExerciseOne/exerciseone.py
+++ ExerciseOne/exerciseone.py
@@ -29,3 +29,41 @@ def rules(n,m):
         return "D"
     elif par(n) and par(m) != True and n<m:
         return "L"
+
+#Función para ingresar los casos de prueba que se desean determinar, en base a las especificaciones.          
+
+def icp():
+    result = []
+    while True:
+        try:
+            t = int(input("Ingrese casos de prueba:\n"))
+            if 1 <= t <=5000:
+                for i in range(t):
+                    print("Caso de prueba " + str(i))
+                    n = int(input("Ingrese n:\n"))
+                    if 1 <= n <= 10**9: 
+                        m = int(input("Ingrese m:\n"))
+                        if 1 <= m <= 10**9:  
+                            result.append(rules(n,m))
+                        else:
+                            print("Error número m no válido.")
+                            print("El número debe estar entre 1 y 10^9.")
+                            exit()
+                    else:
+                        print("Error número n no válido.")
+                        print("El número debe estar entre 1 y 10^9.")
+                        exit()
+                print("----Resultados----\n")
+                for i in result:    
+                    print(i)
+                exit()
+            else:
+                print("Error número de casos de prueba no válido")
+                print("El número debe estar entre 1 y 5000.")
+        except ValueError:
+                print("ATENCIÓN: Debe ingresar un número.")
+
+#Ejecución de icp()
+#Para ejecutar las pruebas unitarias a las funciones comentar la siguiente línea. 
+
+icp()
